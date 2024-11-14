745fadacb97723cc5c7e599ddbdd700a4537624d
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-14 11:23:38 +0100

    added main method: convertor tested and works on user input.

--- convertor.py
+++ convertor.py
@@ -1,5 +1,3 @@
-from logging import raiseExceptions
-from webbrowser import Error
 
 
 class UnitConvertor:
@@ -14,13 +12,15 @@ class UnitConvertor:
         'ounce': 28_349.5, 'pound': 453_592
     }
 
-    temperature_units = {'Celsius', 'Fahrenheit', 'Kelvin'}
+    temperature_units = {'celsius', 'fahrenheit', 'kelvin'}
 
 
     def convert_length(self, value: float, from_unit: str, to_unit:str)-> float:
         """Converts length units - supporting the units in length_units"""
+        from_unit = from_unit.strip().lower()
+        to_unit = to_unit.strip().lower()
         try:
-            return value * (self.length_units[to_unit] / self.length_units[from_unit])
+            return value * (self.length_units[from_unit] / self.length_units[to_unit])
         except KeyError:
             raise ValueError(f'Invalid units: `{from_unit}` or `{to_unit}` is not supported')
         except TypeError:
@@ -29,8 +29,10 @@ class UnitConvertor:
 
     def convert_weight(self, value: float, from_unit: str, to_unit: str)-> float:
         """Converts weight units - supporting the units in weight_units"""
+        from_unit = from_unit.strip().lower()
+        to_unit = to_unit.strip().lower()
         try:
-            return value * (self.length_units[to_unit] / self.length_units[from_unit])
+            return value * (self.weight_units[from_unit] / self.weight_units[to_unit])
         except KeyError:
             raise ValueError(f'Invalid units: `{from_unit}` or `{to_unit}` is not supported.')
         except TypeError:
@@ -39,29 +41,57 @@ class UnitConvertor:
 
     def convert_temperature(self, value: float, from_unit: str, to_unit: str) -> float:
         """Converts temperature units - supporting the units in temperature_units with specific formulas"""
-        try:
-            if from_unit=='Celsius' and to_unit == 'Fahrenheit':
-                return (value * 9/5) + 32
-            elif from_unit=='Fahrenheit' and to_unit == 'Celsius':
-                return (value - 32) * 5/9
-            elif from_unit=='Kelvin' and to_unit == 'Celsius':
-                return value - 273.15
-            elif from_unit=='Celsius' and to_unit == 'Kelvin':
-                return value + 273.15
-            elif from_unit=='Fahrenheit' and to_unit == 'Kelvin':
-                return (value - 32) * 5/9 + 273.15
-            elif from_unit=='Kelvin' and to_unit == 'Fahrenheit':
-                return (value - 273.15) * 9/5 + 32
-        except KeyError:
+        from_unit = from_unit.strip().lower()
+        to_unit = to_unit.strip().lower()
+        if from_unit not in self.temperature_units or to_unit not in self.temperature_units:
             raise ValueError(f'Invalid units: `{from_unit}` or `{to_unit}` is not supported.')
+
+        try:
+            if from_unit=='celsius' and to_unit == 'fahrenheit':
+                return (value * 9/5) + 32
+            elif from_unit=='fahrenheit' and to_unit == 'celsius':
+                return (value - 32) * 5/9
+            elif from_unit=='kelvin' and to_unit == 'celsius':
+                return value - 273.15
+            elif from_unit=='celsius' and to_unit == 'kelvin':
+                return value + 273.15
+            elif from_unit=='fahrenheit' and to_unit == 'kelvin':
+                return (value - 32) * 5/9 + 273.15
+            elif from_unit=='kelvin' and to_unit == 'fahrenheit':
+                return (value - 273.15) * 9/5 + 32
         except TypeError:
             raise TypeError('Invalid input value. Please enter a numeric value(integer or float).')
 
 
+    def convert(self, conversion_type: str, value: float, from_unit: str, to_unit: str):
+        """Handles the conversion type to use the right conversion method"""
+        match conversion_type:
+            case 'length':
+                return self.convert_length(value, from_unit, to_unit)
+            case 'weight':
+                return self.convert_weight(value, from_unit, to_unit)
+            case 'temperature':
+                return self.convert_temperature(value, from_unit, to_unit)
+            case _:
+                raise Exception('Invalid conversion type.')
 
 def main():
     convertor = UnitConvertor()
-    pass
+    conversion_type = input("Enter conversion type (length, weight, temperature): ").strip().lower()
+    try:
+        value = float(input(f"Enter the value to convert: "))
+    except ValueError:
+        print('Invalid input value. Please enter a numeric value(integer or float).')
+        return
+
+    from_unit = input("Enter unit to convert from: ").strip().lower()
+    to_unit = input("Enter unit to convert to: ").strip().lower()
+
+    try:
+        result = convertor.convert(conversion_type, value, from_unit, to_unit)
+        print(f"{value} {from_unit} is equal to {result} {to_unit}.")
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == '__main__':
